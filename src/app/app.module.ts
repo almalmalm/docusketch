3b62f8bc212e60919a9b7704a25b3abb64dd9964
almalmalm <almostkalinin@gmail.com>
2023-07-15 23:40:56 +0400

    feat: add icon generator component and basic styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconGeneratorComponent } from './components/icon-generator/icon-generator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IconGeneratorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/icon-generator/icon-generator.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-generator',
+  templateUrl: './icon-generator.component.html',
+  styleUrls: ['./icon-generator.component.css']
+})
+export class IconGeneratorComponent {
+
+}
