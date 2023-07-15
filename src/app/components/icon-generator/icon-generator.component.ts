81a00a8e019b18dba5601d0c5bc0a72535c2503b
almalmalm <almostkalinin@gmail.com>
2023-07-16 00:19:24 +0400

    feat: implements random icons algorithm

--- src/app/components/icon-generator/icon-generator.component.ts
+++ src/app/components/icon-generator/icon-generator.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-icon-generator',
@@ -7,5 +7,18 @@ import { faCoffee } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./icon-generator.component.css'],
 })
 export class IconGeneratorComponent {
-  faCoffee = faCoffee;
+  faIcons = fas;
+
+  constructor() {
+    console.log(
+      this.randomKey[Math.floor(Math.random() * this.randomKey.length)]
+    );
+    console.log(this.randomIcon);
+    console.log(this.finally);
+  }
+  randomKey = Object.keys(fas);
+  randomIcon =
+    this.randomKey[Math.floor(Math.random() * this.randomKey.length)];
+
+  finally = fas[`${this.randomIcon}`];
 }
